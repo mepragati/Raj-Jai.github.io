25f5c1f00209e4766471940aa485218c16654ff3
Jai Raj <jairaj0616@gmail.com>
2019-02-11 23:26:13 +0530

    update

--- script.js
+++ script.js
@@ -33,3 +33,25 @@ function sanskrit() {
 function english() {
     alert("Syllabus :- \n Litreture - Unit - 9,10,11,12 Q&A BtL \n M.C.B - Unit - 11 to 15 Q&A BtL \n Gra/W.B/Electra - choose the correct words,Sound of things ,Letter,Story-Writing,error,E-mail,Article,Revision,Direct and Indirect speech.");
 }
+
+ function random() {
+            document.getElementById("love").innerHTML = (concat);
+
+            if ((n == 1 && h > 9) || (n == 2 && h <= 9)) {
+                document.getElementById("tuesday").classList.add("current");
+            } else if ((n == 2 && h > 9) || (n == 3 && h <= 9)) {
+                document.getElementById("wednesday").classList.add("current");
+            } else if ((n == 3 && h > 9) || (n == 4 && h <= 9)) {
+                document.getElementById("thursday").classList.add("current");
+            } else if ((n == 4 && h > 9) || (n == 5 && h <= 9)) {
+                document.getElementById("friday").classList.add("current");
+            } else if ((n == 5 && h > 9) || (n == 6 && h <= 9)) {
+                document.getElementById("saturday").classList.add("current");
+            } else if ((n == 6 && h > 9) || (n == 6 && h <= 23)) {
+                document.getElementById("sunday").innerHTML = "Enjoy! tommorow is Sunday.";
+            } else if ((n == 0 && h > 9) || (n == 1 && h <= 9)) {
+                document.getElementById("monday").classList.add("current");
+            } else if ((n == 0 && h >= 0) || (n == 0 && h <= 8)) {
+                document.getElementById("sunday").innerHTML = "Enjoy! today is Sunday.";
+            }
+        }
