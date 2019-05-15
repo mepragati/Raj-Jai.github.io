ce573b9bb219c57f74f5dd1b459f89f61d0b389d
Jai Raj <44637563+Raj-Jai@users.noreply.github.com>
2019-05-15 23:22:52 +0530

    update

--- script.js
+++ script.js
@@ -3,30 +3,34 @@ var rand = Math.floor(Math.random() * myArray.length);
 var concat = myArray[rand];
 var d = new Date();
 
-        var n = d.getDay();
+var n = d.getDay();
 
-        var g = new Date();
+var g = new Date();
 
-        var h = g.getHours();
+var h = g.getHours();
 
-        function random() {
-            document.getElementById("love").innerHTML = (concat);
+function random() {
+    document.getElementById("love").innerHTML = (concat);
 
-            if ((n == 1 && h > 9) || (n == 2 && h <= 9)) {
-                document.getElementById("tuesday").classList.add("current");
-            } else if ((n == 2 && h > 9) || (n == 3 && h <= 9)) {
-                document.getElementById("wednesday").classList.add("current");
-            } else if ((n == 3 && h > 9) || (n == 4 && h <= 9)) {
-                document.getElementById("thursday").classList.add("current");
-            } else if ((n == 4 && h > 9) || (n == 5 && h <= 9)) {
-                document.getElementById("friday").classList.add("current");
-            } else if ((n == 5 && h > 9) || (n == 6 && h <= 9)) {
-                document.getElementById("saturday").classList.add("current");
-            } else if ((n == 6 && h > 9) || (n == 6 && h <= 23)) {
-                document.getElementById("sunday").innerHTML = "Enjoy! tommorow is Sunday.";
-            } else if ((n == 0 && h > 9) || (n == 1 && h <= 9)) {
-                document.getElementById("monday").classList.add("current");
-            } else if ((n == 0 && h >= 0) || (n == 0 && h <= 8)) {
-                document.getElementById("sunday").innerHTML = "Enjoy! today is Sunday.";
-            }
-        }
+    if ((n == 1 && h > 9) || (n == 2 && h <= 9)) {
+        document.getElementById("tuesday").classList.add("current");
+    } else if ((n == 2 && h > 9) || (n == 3 && h <= 9)) {
+        document.getElementById("wednesday").classList.add("current");
+    } else if ((n == 3 && h > 9) || (n == 4 && h <= 9)) {
+        document.getElementById("thursday").classList.add("current");
+    } else if ((n == 4 && h > 9) || (n == 5 && h <= 9)) {
+        document.getElementById("friday").classList.add("current");
+    } else if ((n == 5 && h > 9) || (n == 6 && h <= 9)) {
+        document.getElementById("saturday").classList.add("current");
+    } else if ((n == 6 && h > 9) || (n == 6 && h <= 23)) {
+        document.getElementById("sunday").innerHTML = "Enjoy! tommorow is Sunday.";
+    } else if ((n == 0 && h > 9) || (n == 1 && h <= 9)) {
+        document.getElementById("monday").classList.add("current");
+    } else if ((n == 0 && h >= 0) || (n == 0 && h <= 8)) {
+        document.getElementById("sunday").innerHTML = "Enjoy! today is Sunday.";
+    }
+}
+
+function swapStyleSheet(sheet) {
+    document.getElementById('pagestyle').setAttribute('href', sheet);
+}
